19478f7994446468f2b945adb415f789aa9d379e
shihuanhuan472 <1901491599@qq.com>
2024-07-13 11:22:57 +0800

    两个界面

--- client/src/routers/router.js
+++ client/src/routers/router.js
@@ -19,6 +19,8 @@ import ChangeSelfPassword from "../pages/Personal/PersonalPage/ChangePassword.vu
 import ChangeSelfEmile from "../pages/Personal/PersonalPage/ChangeEmile.vue"
 import ChangeSelfInfo from "../pages/Personal/PersonalPage/ChangeSelfInfo.vue"
 import MessageList from '../pages/MessageList.vue'
+import Recharge from '../pages/Personal/PersonalPage/Recharge.vue'
+import CreateStore from '../pages/Personal/PersonalPage/CreateStore.vue'
 // 创建路由对象,声明路由规则
 const router = createRouter({
 
@@ -106,7 +108,17 @@ const router = createRouter({
                         path:'/changeselfinfo',
                         name:'ChangeSelfInfo',
                         component:ChangeSelfInfo
-                    }
+                    },
+                    {
+                        path:'/recharge',
+                        name:'Recharge',
+                        component:Recharge
+                    },
+                    {
+                        path:'/createstore',
+                        name:'CreateStore',
+                        component:CreateStore
+                    },
                 ]
               }
             ]  
