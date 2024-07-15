f555f8871b04729cd2e643ba15883b0cb51df41d
‘wjx’ <392117998@qq.com>
2024-07-15 10:10:24 +0800

    首页展示

--- client/src/pages/MallHome.vue
+++ client/src/pages/MallHome.vue
@@ -1,16 +1,15 @@
 <template>
     <div class="home_contain">
-       <el-carousel :interval="4000" type="card" height="200px">
-           <el-carousel-item key="1">
-               <img src="../assets/img/banner1.jpg" alt="1" style="width: 100%; height: 100%;">
-           </el-carousel-item>
-           <el-carousel-item key="2">
-               <img src="../assets/img/banner2.jpg" alt="2" style="width: 100%; height: 100%;">
-           </el-carousel-item>
-           <el-carousel-item key="3">
-               <img src="../assets/img/banner3.jpg" alt="3" style="width: 100%; height: 100%;">
-           </el-carousel-item>
-       </el-carousel>
+        <el-carousel :interval="4000" type="card" height="200px">
+            <el-carousel-item
+                v-for="(imageData, index) in CarouselImg"
+                :key="index"
+                >
+            <img :src="imageData.image" alt="Image" style="width: 100%; height: 100%;">
+            </el-carousel-item>
+        </el-carousel>
+        
+
        <div class="home_sort">
            <div class="home_sort_head" style="display: flex; justify-content: space-between; align-items: center;">
                <div class="custom-font">商品分类</div>
@@ -18,135 +17,51 @@
            </div>
            <div class="home_sort_contain">
                <el-space wrap :size="25">
-                   <el-card class="box-card" style="width: 300px">
-                      <div class="contain_img" style="height:80px"><img src="../assets/img/banner1.jpg" style="width: 100%; height: 100%;"></div>
-                      <div class="contain_img_detail" style=" height:70px">
-                       <div style="font-size:medium;font-weight: bold;margin-bottom: 3px;margin-top: 3px;">电子产品</div>
-                       <div style="font-size:small;margin-bottom: 2px;">描述描述描述</div>
-                       <el-link type="info">查看详情</el-link>
-                      </div>
-                   </el-card>
-                   <el-card class="box-card" style="width: 300px">
-                      <div class="contain_img" style="height:80px"><img src="../assets/img/banner3.jpg" style="width: 100%; height: 100%;"></div>
-                      <div class="contain_img_detail" style=" height:70px">
-                       <div style="font-size:medium;font-weight: bold;margin-bottom: 3px;margin-top: 3px;">电子产品</div>
-                       <div style="font-size:small;margin-bottom: 2px;">描述描述描述</div>
-                       <el-link type="info">查看详情</el-link>
-                      </div>
-                   </el-card>
-                   <el-card class="box-card" style="width: 300px">
-                      <div class="contain_img" style="height:80px"><img src="../assets/img/banner2.jpg" style="width: 100%; height: 100%;"></div>
-                      <div class="contain_img_detail" style=" height:70px">
-                       <div style="font-size:medium;font-weight: bold;margin-bottom: 3px;margin-top: 3px;">电子产品</div>
-                       <div style="font-size:small;margin-bottom: 2px;">描述描述描述</div>
-                       <el-link type="info">查看详情</el-link>
-                      </div>
-                   </el-card>
-                   <el-card class="box-card" style="width: 300px">
-                      <div class="contain_img" style="height:80px"><img src="../assets/img/banner2.jpg" style="width: 100%; height: 100%;"></div>
-                      <div class="contain_img_detail" style=" height:70px">
-                       <div style="font-size:medium;font-weight: bold;margin-bottom: 3px;margin-top: 3px;">电子产品</div>
-                       <div style="font-size:small;margin-bottom: 2px;">描述描述描述</div>
-                       <el-link type="info">查看详情</el-link>
-                      </div>
-                   </el-card>
+                <el-card v-for="(category, index) in CategoriesRecommend" :key="index" class="box-card" style="width: 300px">
+                    <div class="contain_img" style="height:80px">
+                        <img :src="category.image" style="width: 100%; height: 100%;">
+                    </div>
+                    <div class="contain_img_detail" style=" height:70px">
+                        <div style="font-size:medium;font-weight: bold;margin-bottom: 3px;margin-top: 3px;">
+                        {{category.name}}
+                        </div>
+                        <div 
+                        style="font-size:small;
+                        margin-bottom: 2px;
+                        overflow: hidden;  
+                        text-overflow: ellipsis;  
+                        display: -webkit-box;  
+                        -webkit-line-clamp: 2; 
+                        -webkit-box-orient: vertical;  
+                        line-height: 1.2; 
+                        height: 2.4em; ">
+                            {{category.description}}
+                        </div>
+                       <el-link type="info" @click="handleSort">查看详情</el-link>
+                    </div>
+                </el-card>
                </el-space>
            </div>
        </div>
+
+
        <div class="home_goodthings">
            <div class="custom-font" >
                优品推荐
            </div>
            <div class="home_sort_contain">
                <el-space wrap :size="25">
-                   <el-card class="box-card" style="width: 250px">
-                      <div class="contain_img" style="height:100px"><img src="../assets/img/banner3.jpg" style="width: 100%; height: 100%;"></div>
+                <el-card v-for="(goods, index) in goodsOpts" class="box-card" style="width: 250px">
+                      <div class="contain_img" style="height:100px">
+                        <img :src="goods.image" style="width: 100%; height: 100%;">
+                    </div>
                       <div class="contain_img_detail" style=" height:70px">
-                       <div style="font-size:small;font-weight: bold;margin-bottom: 5px;margin-top: 5px;">电子产品</div>
-                       <div style="font-size:x-small;margin-bottom: 5px;color:red">￥12.05</div>
-                       <el-button type="primary" size="small">加入购物车</el-button>
-                      </div>
-                   </el-card>
-                   <el-card class="box-card" style="width: 250px">
-                      <div class="contain_img" style="height:100px"><img src="../assets/img/banner3.jpg" style="width: 100%; height: 100%;"></div>
-                      <div class="contain_img_detail" style=" height:70px">
-                       <div style="font-size:small;font-weight: bold;margin-bottom: 5px;margin-top: 5px;">电子产品</div>
-                       <div style="font-size:x-small;margin-bottom: 5px;color:red">￥12.05</div>
-                       <el-button type="primary" size="small">加入购物车</el-button>
-                      </div>
-                   </el-card>
-                   <el-card class="box-card" style="width: 250px">
-                      <div class="contain_img" style="height:100px"><img src="../assets/img/banner3.jpg" style="width: 100%; height: 100%;"></div>
-                      <div class="contain_img_detail" style=" height:70px">
-                       <div style="font-size:small;font-weight: bold;margin-bottom: 5px;margin-top: 5px;">电子产品</div>
-                       <div style="font-size:x-small;margin-bottom: 5px;color:red">￥12.05</div>
-                       <el-button type="primary" size="small">加入购物车</el-button>
-                      </div>
-                   </el-card>
-               </el-space>
-           </div>
-       </div>
-       <div class="home_goodthings">
-           <div class="custom-font">
-               品牌推荐
-           </div>
-           <div class="home_sort_contain">
-               <el-space wrap :size="50">
-                   <el-card class="box-card" style="width: 250px">
-                      <div class="contain_img" style="height:100px"><img src="../assets/img/banner1.jpg" style="width: 100%; height: 100%;"></div>
-                      <div class="contain_img_detail" style=" height:70px">
-                       <div style="font-size:small;font-weight: bold;margin-bottom: 5px;margin-top: 5px;">电子产品</div>
-                       <div style="font-size:x-small;margin-bottom: 5px;color:red">￥12.05</div>
-                       <el-button type="primary" size="small">加入购物车</el-button>
-                      </div>
-                   </el-card>
-                   <el-card class="box-card" style="width: 250px">
-                      <div class="contain_img" style="height:100px"><img src="../assets/img/banner2.jpg" style="width: 100%; height: 100%;"></div>
-                      <div class="contain_img_detail" style=" height:70px">
-                       <div style="font-size:small;font-weight: bold;margin-bottom: 5px;margin-top: 5px;">电子产品</div>
-                       <div style="font-size:x-small;margin-bottom: 5px;color:red">￥12.05</div>
-                       <el-button type="primary" size="small">加入购物车</el-button>
-                      </div>
-                   </el-card>
-                   <el-card class="box-card" style="width: 250px">
-                      <div class="contain_img" style="height:100px"><img src="../assets/img/banner3.jpg" style="width: 100%; height: 100%;"></div>
-                      <div class="contain_img_detail" style=" height:70px">
-                       <div style="font-size:small;font-weight: bold;margin-bottom: 5px;margin-top: 5px;">电子产品</div>
-                       <div style="font-size:x-small;margin-bottom: 5px;color:red">￥12.05</div>
-                       <el-button type="primary" size="small">加入购物车</el-button>
-                      </div>
-                   </el-card>
-                   
-               </el-space>
-           </div>
-       </div>
-       <div class="home_goodthings">
-           <div class="custom-font">
-               秒杀专区
-           </div>
-           <div class="home_sort_contain">
-               <el-space wrap :size="50">
-                   <el-card class="box-card" style="width: 250px">
-                      <div class="contain_img" style="height:100px"><img src="../assets/img/banner3.jpg" style="width: 100%; height: 100%;"></div>
-                      <div class="contain_img_detail" style=" height:70px">
-                       <div style="font-size:small;font-weight: bold;margin-bottom: 5px;margin-top: 5px;">电子产品</div>
-                       <div style="font-size:x-small;margin-bottom: 5px;color:red">￥12.05</div>
-                       <el-button type="primary" size="small">加入购物车</el-button>
-                      </div>
-                   </el-card>
-                   <el-card class="box-card" style="width: 250px">
-                      <div class="contain_img" style="height:100px"><img src="../assets/img/banner2.jpg" style="width: 100%; height: 100%;"></div>
-                      <div class="contain_img_detail" style=" height:70px">
-                       <div style="font-size:small;font-weight: bold;margin-bottom: 5px;margin-top: 5px;">电子产品</div>
-                       <div style="font-size:x-small;margin-bottom: 5px;color:red">￥12.05</div>
-                       <el-button type="primary" size="small">加入购物车</el-button>
-                      </div>
-                   </el-card>
-                   <el-card class="box-card" style="width: 250px">
-                      <div class="contain_img" style="height:100px"><img src="../assets/img/banner1.jpg" style="width: 100%; height: 100%;"></div>
-                      <div class="contain_img_detail" style=" height:70px">
-                       <div style="font-size:small;font-weight: bold;margin-bottom: 5px;margin-top: 5px;">电子产品</div>
-                       <div style="font-size:x-small;margin-bottom: 5px;color:red">￥12.05</div>
+                       <div style="font-size:small;font-weight: bold;margin-bottom: 5px;margin-top: 5px;">
+                        {{goods.name}}
+                        </div>
+                       <div style="font-size:x-small;margin-bottom: 5px;color:red">
+                        ￥{{goods.price}}
+                        </div>
                        <el-button type="primary" size="small">加入购物车</el-button>
                       </div>
                    </el-card>
@@ -154,15 +69,181 @@
                </el-space>
            </div>
        </div>
+
+       <div class="home_goodthings">
+           <div class="custom-font">
+               品牌推荐
+           </div>
+           <div class="home_sort_contain">
+               <el-space wrap :size="50">
+                <el-card v-for="(goods, index) in goodsBrands" class="box-card" style="width: 250px">
+                    <div class="contain_img" style="height:100px">
+                        <img :src="goods.image" style="width: 100%; height: 100%;">
+                    </div>
+                      <div class="contain_img_detail" style=" height:70px">
+                       <div style="font-size:small;font-weight: bold;margin-bottom: 5px;margin-top: 5px;">
+                        {{goods.name}}
+                        </div>
+                       <div style="font-size:x-small;margin-bottom: 5px;color:red">
+                        ￥{{goods.price}}
+                        </div>
+                       <el-button type="primary" size="small">加入购物车</el-button>
+                      </div>
+                   </el-card>
+               </el-space>
+           </div>
+       </div>
+
+       <div class="home_goodthings">
+           <div class="custom-font">
+               秒杀专区
+           </div>
+           <div class="home_sort_contain">
+               <el-space wrap :size="50">
+                <el-card v-for="(goods, index) in goodsSpis" class="box-card" style="width: 250px">
+                    <div class="contain_img" style="height:100px">
+                        <img :src="goods.image" style="width: 100%; height: 100%;">
+                    </div>
+                      <div class="contain_img_detail" style=" height:70px">
+                       <div style="font-size:small;font-weight: bold;margin-bottom: 5px;margin-top: 5px;">
+                        {{goods.name}}
+                        </div>
+                       <div style="font-size:x-small;margin-bottom: 5px;color:red">
+                        ￥{{goods.price}}
+                        </div>
+                       <el-button type="primary" size="small">加入购物车</el-button>
+                      </div>
+                   </el-card>
+               </el-space>
+           </div>
+       </div>
     </div>
 </template>
    
 <script setup>
-   import { useRouter } from 'vue-router';
-   const router = useRouter();
-function handleSort(){
-    router.push('/goodlist')
-}
+    import {ref,onMounted} from 'vue'
+    import { useRouter } from 'vue-router';
+    import { goodsGraph,getGoodsById ,goodsOpt,goodsBrand,goodsSpi} from '../api/goods'
+    import { categoryRecommend } from '../api/category'
+
+
+    const router = useRouter();
+
+    const CarouselImg= ref([])
+    const CategoriesRecommend= ref([])
+    const goodsOpts=ref([])
+    const goodsBrands=ref([])
+    const goodsSpis=ref([])
+
+    
+
+    function handleSort(){
+        router.push('/goodlist')
+    }
+
+    async function showCategoryRecommend()
+    {
+        const response = await categoryRecommend()
+        CategoriesRecommend.value = response.data
+    }
+
+    async function showCarouselImg()
+    {
+        const response = await goodsGraph()
+        CarouselImg.value = response.data
+    }
+
+    async function showGoodsOpt()
+    {
+        try {  
+            const response = await goodsOpt();  
+            goodsOpts.value = response.data; 
+    
+            // 创建一个新数组来存储更新后的商品信息  
+            let updatedGoods = [];  
+    
+            // 遍历 goodsOpts 中的每个商品  
+            for (const good of goodsOpts.value) {  
+                const goodDetail = await getGoodsById(good.id);  
+                if (goodDetail && goodDetail.data) {  
+                    good.image = goodDetail.data.image;  
+                    updatedGoods.push(good);  
+                } else {  
+                    console.error(`Failed to fetch details for good with ID: ${good.id}`);  
+                    updatedGoods.push({ ...good, error: "Failed to fetch image" });  
+                }  
+            }   
+            goodsOpts.value = updatedGoods;  
+            console.log('goodsOpts.value', goodsOpts.value);  
+
+        } catch (error) {  
+        console.error('Error updating goods images:', error);  
+        }  
+    }
+
+    async function showGoodsSpi()
+    {
+        try {  
+            const response = await goodsSpi();  
+            goodsSpis.value = response.data; 
+    
+            // 创建一个新数组来存储更新后的商品信息  
+            let updatedGoods = [];  
+    
+            // 遍历 goodsOpts 中的每个商品  
+            for (const good of goodsSpis.value) {  
+                const goodDetail = await getGoodsById(good.id);  
+                if (goodDetail && goodDetail.data) {  
+                    good.image = goodDetail.data.image;  
+                    updatedGoods.push(good);  
+                } else {  
+                    console.error(`Failed to fetch details for good with ID: ${good.id}`);  
+                    updatedGoods.push({ ...good, error: "Failed to fetch image" });  
+                }  
+            }   
+            goodsSpis.value = updatedGoods;  
+            console.log('goodsSpis.value', goodsSpis.value);  
+
+        } catch (error) {  
+        console.error('Error updating goods images:', error);  
+        }  
+    }
+
+    async function showGoodsBrand()
+    {
+        try {  
+            const response = await goodsBrand();  
+            goodsBrands.value = response.data; 
+    
+            // 创建一个新数组来存储更新后的商品信息  
+            let updatedGoods = [];  
+    
+            // 遍历 goodsOpts 中的每个商品  
+            for (const good of goodsBrands.value) {  
+                const goodDetail = await getGoodsById(good.id);  
+                if (goodDetail && goodDetail.data) {  
+                    good.image = goodDetail.data.image;  
+                    updatedGoods.push(good);  
+                } else {  
+                    console.error(`Failed to fetch details for good with ID: ${good.id}`);  
+                    updatedGoods.push({ ...good, error: "Failed to fetch image" });  
+                }  
+            }   
+            goodsBrands.value = updatedGoods;  
+            console.log('goodsBrands.value', goodsBrands.value);  
+
+        } catch (error) {  
+        console.error('Error updating goods images:', error);  
+        }  
+    }
+
+    onMounted(()=>{
+        showCarouselImg()
+        showCategoryRecommend()
+        showGoodsOpt()
+        showGoodsBrand()
+        showGoodsSpi()
+    })
 
 </script>
    
