10f439ca02d0ffd395ee4efb32903d8867218bfc
‘wjx’ <392117998@qq.com>
2024-07-15 10:12:09 +0800

    main.js去掉use axios

--- client/src/main.js
+++ client/src/main.js
@@ -4,7 +4,6 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import router from "./routers/router"
-import axios from 'axios'; 
 
 const pinia = createPinia()
 const app=createApp(App)
@@ -12,5 +11,4 @@ const app=createApp(App)
 app.use(pinia)
 app.use(ElementPlus)
 app.use(router)
-app.use(axios)
 app.mount('#app')
