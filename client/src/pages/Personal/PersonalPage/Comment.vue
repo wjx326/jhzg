a56ccf6756e31f72cfc7af19ec1b1a6f304fb4ca
shihuanhuan472 <1901491599@qq.com>
2024-07-16 19:02:36 +0800

    评论和充值

--- client/src/pages/Personal/PersonalPage/Comment.vue
+++ client/src/pages/Personal/PersonalPage/Comment.vue
@@ -2,39 +2,71 @@
     <div style="width: 98%;background-color: white;margin:20px auto;">
         <div class="order_header" style="margin: 15px;">
             <el-menu
-                :default-active="activeIndex"
+                default-active="1"
                 class="el-menu-demo"
                 mode="horizontal"
-                @select="handleSelect"
             >
-                <el-menu-item index="1">来自卖家的评价</el-menu-item>
-                <el-menu-item index="2">给他人的评价</el-menu-item>
-                <el-menu-item index="3">已处理的评价</el-menu-item>
+                <el-menu-item index="1">给商品的评价</el-menu-item>
             </el-menu>
         </div>
         <div style="margin: 15px;">
             <el-table :data="tableData" style="width: 100%">
-                <el-table-column >
-                    <template #header>
-                        <el-select  placeholder="评价" style="width: 100px">
-                            <el-option label="好评"/>
-                            <el-option label="中评"/>
-                            <el-option label="差评"/>
-                        </el-select>
-                    </template>
-                </el-table-column>
-                <el-table-column >
-                    <template #header>
-                        <el-select  placeholder="评论" style="width: 100px">
-                            <el-option label="有评价内容"/>
-                            <el-option label="无评价内容"/>
-                        </el-select>
-                    </template>
-                </el-table-column>
-                <el-table-column prop="score" label="评价人" width="180" />
-                <el-table-column prop="name" label="宝贝信息" width="180" />
-                <el-table-column prop="address" label="操作" />
+            <el-table-column prop="goodName" label="商品名称" width="180" />
+            <el-table-column prop="goodsScore" label="商品评分" width="180" />
+            <el-table-column label="评价类型" width="180">
+                <template #default="scope">
+                {{ scope.row.isHidden === 0 ? '非匿名' : '匿名' }}
+                </template>
+            </el-table-column>
+            <el-table-column prop="shopName" label="店铺名称" width="180" />
+            <el-table-column prop="shopScore" label="店铺评分" width="180" />
             </el-table>
         </div>
     </div>
 </template>
+<script setup>
+import { onMounted, ref } from 'vue';
+import { commentPage } from '../../../api/comment';
+import { getGoodsById } from '../../../api/goods';
+import { getShopByShopId } from '../../../api/shop';
+
+const tableData = ref([]);
+const currentPage=ref(1);
+const currentPageSize=ref(5)
+
+const fetchCommentData = async () => {
+  try {
+    const response = await commentPage(currentPage.value,currentPageSize.value);
+
+    // 确保response.data.records是一个数组
+    if (!Array.isArray(response.data.records)) {
+      throw new Error('Expected an array from response.data.records');
+    }
+
+    for (let i = 0; i < response.data.records.length; i++) {
+      const c = response.data.records[i];
+      const responseGood = await getGoodsById(c.goods_id);
+      const responseShop = await getShopByShopId(c.shop_id);
+      const goodName = responseGood.data.name;
+      const shopName = responseShop.data.name;
+
+      // 将处理后的数据添加到tableData中
+      tableData.value.push({
+        id: c.id,
+        goodName: goodName,
+        content: c.content,
+        goodsScore: c.goods_score,
+        shopScore: c.shop_score,
+        isHidden: c.is_hidden,
+        shopName: shopName
+      });
+    }
+  } catch (error) {
+    console.error('Error fetching comment data:', error);
+  }
+};
+
+onMounted(() => {
+  fetchCommentData();
+});
+</script>
