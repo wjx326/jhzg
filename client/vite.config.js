cae85cd6fb5155b9d441925c6bf4b3b2cd74bbb9
‘wjx’ <392117998@qq.com>
2024-07-17 08:46:22 +0800

    配置代理

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:2019',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
