a555329deebd4ad170ce3fb955b4b72fdbdb9038
‘wjx’ <392117998@qq.com>
2024-07-18 14:48:50 +0800

    整合

--- client/src/main.js
+++ client/src/main.js
@@ -4,10 +4,13 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import router from "./routers/router"
+import  VCharts  from 'v-charts'
+
 
 const pinia = createPinia()
 const app=createApp(App)
 
+app.use(VCharts)
 app.use(pinia)
 app.use(ElementPlus)
 app.use(router)
